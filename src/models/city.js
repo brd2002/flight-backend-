92aa5929051ebc432c6ea567f15015c91695bff6
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2025-06-20 16:18:38 +0530

    foreign key setup -> airport cityid with City id done

--- src/models/city.js
+++ src/models/city.js
@@ -11,7 +11,6 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-       City.hasMany(models.Airport, { foreignKey: 'cityId' });
     }
   }
   City.init({
